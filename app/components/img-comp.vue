fdd53071fa708d6fb26664459aece7f8b6a26940
alexlyaps <a34270622@gmail.com>
2025-08-14 11:22:38 +0200

    feat/img-comp

---
+++ app/components/img-comp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="img-wrapper"
+    :style="`width: clamp(${width / 3}px, ${
+      width / 19.2
+    }vw, ${width}px); aspect-ratio: ${width / height}`"
+  >
+    <img :src="src" :alt="alt" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  src: string;
+  alt?: string;
+  width: number;
+  height: number;
+}>();
+</script>
+
+<style scoped lang="scss">
+// базовый стиль
+
+img {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+</style>
